2c17f2f6c373630fa5c722482e971e8c2fffdbcf
Dilara <dcavus@gmail.com>
2023-03-06 13:13:40 -0800

    Python basics added

---
+++ 01_Strings.py
@@ -0,0 +1,18 @@
+myvar="hello"
+print(myvar.upper())
+myvar="Hello Mike, how is your dog?"
+print(myvar.split())
+print(myvar.split(","))
+print(myvar.split("o"))
+# charAt
+print(myvar[0])
+# indexing can go backwards
+print(myvar[-1])
+
+# substring
+print(myvar[0:5])
+
+first_name="Dilara";
+last_name=""
+print(f"Hello{first_name} {last_name}")
+
